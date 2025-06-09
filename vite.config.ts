bc6ee4af04bb6cb03fa3f9152d7dfce6a1400f0a
MarkDev232 <marksauquillo3@gmail.com>
2025-06-09 22:36:06 +0800

    sdsds

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace this with your actual GitHub repo name
-const repoName = "cloud-cast-liveview";
+
 
 export default defineConfig(({ mode }) => ({
-  base: `/${repoName}/`, // 👈 IMPORTANT for GitHub Pages
+  base: `/cloud-cast-liveview/`, // 👈 IMPORTANT for GitHub Pages
   server: {
     host: "::",
     port: 8080,
