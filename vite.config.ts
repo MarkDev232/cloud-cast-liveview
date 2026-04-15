725fb77b88ba08f4de5966f66d0486de09a0829f
MarkDev232 <marksauquillo3@gmail.com>
2026-04-15 15:22:13 +0800

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/cloud-cast-liveview/", // Must match your repository name exactly
+  base: "/", // Must match your repository name exactly
   server: {
     host: "::",
     port: 8080,
