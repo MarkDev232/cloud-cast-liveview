4ab845244760557d0212953ddb40edc9f4477aa5
MarkDev232 <marksauquillo3@gmail.com>
2026-04-15 15:56:47 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,33 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/cloud-cast-liveview/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const isGitHubPages = mode === "gh-pages";
+
+  return {
+    base: isGitHubPages ? "/cloud-cast-liveview/" : "/",
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-  // Add these new build options
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-    target: "esnext", // Add this line
-  },
+    build: {
+      outDir: "dist",
+      emptyOutDir: true,
+      assetsDir: "assets",
+      rollupOptions: {
+        output: {
+          assetFileNames: "assets/[name]-[hash][extname]",
+          entryFileNames: "assets/[name]-[hash].js",
+          chunkFileNames: "assets/[name]-[hash].js",
+        },
+      },
+      target: "esnext",
+    },
+  };
 });
